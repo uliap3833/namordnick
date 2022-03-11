c36bcea083330e145351ff6147edefbeed03e012
uliap3833@gmail.com <uliap3833@gmail.com>
2022-03-12 01:54:46 +0300

    init

---
+++ Namordnik/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Namordnik
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Product> DB;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComboBoxFilt.Items.Add("Все");
+            foreach (ProductType p in db.dbcon.ProductType.ToList())
+            {
+                ComboBoxFilt.Items.Add(p.Title);
+            }
+            ComboBoxFilt.SelectedIndex = 0;
+            ComboBoxSort.SelectedIndex = 0;
+        }
+
+        private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filt();
+        }
+
+        private void ComboBoxSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filt();
+        }
+
+        public void Filt(bool isUp = true)
+        {
+            List<Product> newDB = new List<Product>();
+            ViewDB.Items.Clear();
+            DB = db.dbcon.Product.ToList();
+            string searchStroke = TextBoxSearch.Text.ToLower();
+            if (searchStroke.Length > 0)
+            {
+                foreach (Product p in DB)
+                {
+                    if (p.Title.ToLower().Contains(searchStroke))
+                    {
+                        newDB.Add(p);
+                    }
+                }
+                DB = newDB;
+            }
+
+            if (ComboBoxFilt.SelectedValue.ToString() != "Все")
+            {
+                DB = DB.Where(x => x.ProductType.Title.ToString() == ComboBoxFilt.SelectedValue.ToString()).ToList();
+            }
+
+            switch(ComboBoxSort.SelectedIndex)
+            {
+                case 0:
+                    if(isUp)
+                    {
+                        DB = DB.OrderBy(x => x.Title).ToList();
+                    } else
+                    {
+                        DB = DB.OrderByDescending(x => x.Title).ToList();
+                    }
+                    break;
+                case 1:
+                    if (isUp)
+                    {
+                        DB = DB.OrderBy(x => x.ProductionWorkshopNumber).ToList();
+                    }
+                    else
+                    {
+                        DB = DB.OrderByDescending(x => x.ProductionWorkshopNumber).ToList();
+                    }
+                    break;
+                case 2:
+                    if (isUp)
+                    {
+                        DB = DB.OrderBy(x => x.MinCostForAgent).ToList();
+                    }
+                    else
+                    {
+                        DB = DB.OrderByDescending(x => x.MinCostForAgent).ToList();
+                    }
+                    break;
+            }
+
+            foreach (Product p in DB)
+            {
+                ViewDB.Items.Add(p);
+            }
+        }
+
+        public void BtnUp_Click(object sender, RoutedEventArgs e)
+        {
+            Filt();
+        }
+
+        public void BtnDown_Click(object sender, RoutedEventArgs e)
+        {
+            Filt(false);
+        }
+
+        private void ViewDB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ViewDB.SelectedItems.Count>0)
+            {
+                BtnChangePrice.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnChangePrice.Visibility = Visibility.Collapsed;
+            }
+            if(ViewDB.SelectedItems.Count==1)
+            {
+                BtnChange.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnChange.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void BtnChangePrice_Click(object sender, RoutedEventArgs e)
+        {
+            
+            List<Product> ppp = new List<Product>();
+            foreach(Product pp in ViewDB.SelectedItems)
+            {
+                ppp.Add(pp);
+            }
+            ChangePriceForAgent window = new ChangePriceForAgent(ppp as List<Product>);
+            window.Owner = this;
+            window.Visibility = Visibility.Visible;
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.ShowDialog();
+            ViewDB.Items.Refresh();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddoOrRedactProduct window = new AddoOrRedactProduct();
+            window.Owner = this;
+            window.Visibility = Visibility.Visible;
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.ShowDialog();
+            Filt();
+        }
+
+        private void BtnChange_Click(object sender, RoutedEventArgs e)
+        {
+            Product p = (Product)ViewDB.SelectedItem;
+            AddoOrRedactProduct window = new AddoOrRedactProduct(p);
+            window.Owner = this;
+            window.Visibility = Visibility.Visible;
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.ShowDialog();
+            Filt();
+        }
+    }
+}
---
+++ Namordnik/db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Namordnik
+{
+    class db
+    {
+        public static Entities1 dbcon = new Entities1();
+    }
+}
